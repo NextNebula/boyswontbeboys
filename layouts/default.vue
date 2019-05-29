b9b716380076acf788edc8cae97ec1e5ff455285
Richard Leurs <leursrichard@gmail.com>
2019-05-29 17:08:35 +0200

    Improve initial website.
    - Add fading scroll arrow.
    - Add info about calling.
    - Add bottom banner.

--- layouts/default.vue
+++ layouts/default.vue
@@ -7,13 +7,18 @@
 <style>
 html {
   font-family: 'Nunito', sans-serif;
-  font-size: 16px;
+  font-size: 18px;
   word-spacing: 1px;
   -ms-text-size-adjust: 100%;
   -webkit-text-size-adjust: 100%;
   -moz-osx-font-smoothing: grayscale;
   -webkit-font-smoothing: antialiased;
   box-sizing: border-box;
+  color: #605e5e;
+}
+
+a {
+  color: #605e5e;
 }
 
 *,
