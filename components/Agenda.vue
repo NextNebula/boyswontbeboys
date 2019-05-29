5df5dfc7fc6e1c0e29371728a0a09867452a4d1c
Richard Leurs <leursrichard@gmail.com>
2019-05-29 16:26:27 +0200

    Initial website.

---
+++ components/Agenda.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="agenda">
+    <div class="month">
+      <div class="month-header">AUGUSTUS</div>
+      <div class="agenda-item">8,9,10 Boulevard Festival Den Bosch (landelijke première, data nnb)</div>
+    </div>
+    <div class="month">
+      <div class="month-header">SEPTEMBER</div>
+      <div class="agenda-item">do 12 De Nieuwe Vorst Tilburg</div>
+      <div class="agenda-item">vr 13 De Nieuwe Vorst Tilburg</div>
+      <div class="agenda-item">za 14 De Nieuwe Vorst Tilburg</div>
+      <div class="agenda-item">do 19 Stadsschouwburg Utrecht</div>
+    </div>
+    <div class="month">
+      <div class="month-header">OKTOBER</div>
+      <div class="agenda-item">za 5 Walhalla Rotterdam</div>
+      <div class="agenda-item">wo 9 Wilminktheater Enschede</div>
+      <div class="agenda-item">vr 11 KAF Almere</div>
+      <div class="agenda-item">di 15 Theater Bellevue Amsterdam</div>
+      <div class="agenda-item">wo 16 Theater Bellevue Amsterdam</div>
+      <div class="agenda-item">do 17 Theater Bellevue Amsterdam</div>
+      <div class="agenda-item">vr 18 Theater Bellevue Amsterdam</div>
+      <div class="agenda-item">za 19 Theater Bellevue Amsterdam</div>
+    </div>
+    <div class="month">
+      <div class="month-header">NOVEMBER</div>
+      <div class="agenda-item">do 14 Theater aan de Rijn Arnhem</div>
+      <div class="agenda-item">za 16 Jonge Harten festival Groningen</div>
+      <div class="agenda-item">zo 17 Jonge Harten festival Groningen</div>
+      <div class="agenda-item">di 19 Zaal 3 Den Haag (promo)</div>
+    </div>
+    <div class="month">
+      <div class="month-header">DECEMBER</div>
+      <div class="agenda-item">di 3 Zaal 3 Den Haag</div>
+    </div>
+    <div class="month">
+      <div class="month-header">JANUARI 2020</div>
+      <div class="agenda-item">vr 10 Chassé theater Breda</div>
+    </div>
+  </div>
+</template>
+
+<style>
+  .agenda div:last-of-type {
+    margin-bottom: 0;
+  }
+
+  .month {
+    margin-bottom: 1rem;
+  }
+
+  .month-header {
+    font-weight: 600;
+  }
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,76 @@
+<template>
+  <div>
+    <div class="container container-banner"></div>
+      <section class="container container-overview">
+        <span class="title">OVER DE SHOW</span>
+        <span class="text">
+        Boys won't be boys van Rikkert Huisstede is een maatschappelijke beweging en theatervoorstelling die een diverse groep jongens en mannen samenbrengt.
+        Een collage van dansers, zangers, rappers, dichters en theatermakers laat zien dat het stoer is om je als man kwetsbaar op te stellen.
+        Ze vertellen hun persoonlijke verhaal en vinden elkaar in het doorbreken van stereotypen. 
+        <br>
+        <br>
+        We hebben lieve jongens nodig. Jongens die niet aan het mannelijkheidsideaal voldoen, denken vaak dat ze iets fout doen. Ze denken dat ze mannelijker zouden moeten zijn.
+        Maar in tijden van #metoo, homofobie, seksisme, racisme en geweld zijn zij misschien juist een oplossing? Onze wereld heeft behoefte aan liefde van mannen.
+        Wij kunnen laten zien dat het anders kan door hokjes te doorbreken en ruimte te maken om onszelf te zijn.
+        </span>
+      </section>
+      <section class="container container-agenda">
+        <span class="title">SPEELLIJST</span>
+        <agenda/>
+      </section>
+  </div>
+</template>
+
+<script>
+import Agenda from '~/components/Agenda.vue'
+
+export default {
+  components: {
+    Agenda
+  }
+}
+</script>
+
+<style>
+.container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  z-index: 100;
+  padding: 2rem 1rem 2rem 1rem;
+  color: #605E5E;
+}
+
+.container-banner {
+  background-image: url('~assets/banner.jpg');
+  width: 100vw;
+  height: calc(100vh - 50px);
+  background-attachment: fixed;
+  background-position: top;
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+
+.container-overview {
+  background-color: #EDEDE4;
+  color: #605E5E;
+}
+
+.container-agenda {
+  background-color: #FAFAFA;
+}
+
+.title {
+  font-size: 24px;
+  margin-bottom: 1rem;
+  font-weight: 600;
+  line-height: 1;
+}
+
+.text {
+  font-size: 18px;
+  max-width: 900px;
+}
+</style>
