a85659496cd69db0e5b424925e800ba128e0a015
Richard Leurs <leursrichard@gmail.com>
2019-06-20 00:07:48 +0200

    Add Facebook links to agenda.

--- components/Agenda.vue
+++ components/Agenda.vue
@@ -9,18 +9,21 @@
         <td>Den Bosch</td>
         <td>Boulevard Festival</td>
         <td><a href="https://www.festivalboulevard.nl/nl/programma/id-4323/boys-won't-be-boys/" target="_blank">Tickets</a></td>
+        <td><a href="https://www.facebook.com/events/396312284427421/" target="_blank">Facebook</a></td>
       </tr>
       <tr>
         <td>vr 9</td>
         <td>Den Bosch</td>
         <td>Boulevard Festival</td>
         <td><a href="https://www.festivalboulevard.nl/nl/programma/id-4323/boys-won't-be-boys/" target="_blank">Tickets</a></td>
+        <td><a href="https://www.facebook.com/events/396312284427421/" target="_blank">Facebook</a></td>
       </tr>
       <tr>
         <td>za 10</td>
         <td>Den Bosch</td>
         <td>Boulevard Festival</td>
         <td><a href="https://www.festivalboulevard.nl/nl/programma/id-4323/boys-won't-be-boys/" target="_blank">Tickets</a></td>
+        <td><a href="https://www.facebook.com/events/396312284427421/" target="_blank">Facebook</a></td>
       </tr>
       <tr class="month-header">
         <td colspan="4">SEPTEMBER 2019</td>
@@ -30,24 +33,28 @@
         <td>Tilburg</td>
         <td>De Nieuwe Vorst</td>
         <td><a href="https://denieuwevorst.nl/programma/boys-wont-be-boys" target="_blank">Tickets</a></td>
+        <td><a href="https://www.facebook.com/events/371883650111451/" target="_blank">Facebook</a></td>
       </tr>
       <tr>
         <td>vr 13</td>
         <td>Tilburg</td>
         <td>De Nieuwe Vorst</td>
         <td><a href="https://denieuwevorst.nl/programma/boys-wont-be-boys" target="_blank">Tickets</a></td>
+        <td><a href="https://www.facebook.com/events/371883650111451/" target="_blank">Facebook</a></td>
       </tr>
       <tr>
         <td>za 14</td>
         <td>Tilburg</td>
         <td>De Nieuwe Vorst</td>
         <td><a href="https://denieuwevorst.nl/programma/boys-wont-be-boys" target="_blank">Tickets</a></td>
+        <td><a href="https://www.facebook.com/events/371883650111451/" target="_blank">Facebook</a></td>
       </tr>
       <tr>
         <td>do 19</td>
         <td>Utrecht</td>
         <td>Stadsschouwburg</td>
         <td><a href="https://stadsschouwburg-utrecht.nl/agenda/2019-20/boys-wont-be-boys/" target="_blank">Tickets</a></td>
+        <td><a href="https://www.facebook.com/events/340907676622217/" target="_blank">Facebook</a></td>
       </tr>
       <tr class="month-header">
         <td colspan="4">OKTOBER 2019</td>
@@ -57,48 +64,56 @@
         <td>Rotterdam</td>
         <td>Walhalla</td>
         <td><a href="https://www.theaterwalhalla.nl/voorstelling/boys-wont-be-boys/" target="_blank">Tickets</a></td>
+        <td><a href="https://www.facebook.com/events/387637238519430/" target="_blank">Facebook</a></td>
       </tr>
       <tr>
         <td>wo 9</td>
         <td>Enschede</td>
         <td>Wilminktheater</td>
         <td><a href="https://www.wilminktheater.nl/programma/!/4987/zou-jij-het-zijn-o.l.v.-rikkert-van-huisstede/boys-wont-be-boys/" target="_blank">Tickets</a></td>
+        <td><a href="https://www.facebook.com/events/681580705639802/" target="_blank">Facebook</a></td>
       </tr>
       <tr>
         <td>vr 11</td>
         <td>Almere</td>
         <td>KAF</td>
         <td><a href="https://kaf.nl/podium/agenda/seizoen-2019-2020/boys-wont-be-boys/" target="_blank">Tickets</a></td>
+        <td><a href="https://www.facebook.com/events/295335558038314/" target="_blank">Facebook</a></td>
       </tr>
       <tr>
         <td>di 15</td>
         <td>Amsterdam</td>
         <td>Theater Bellevue</td>
         <td><a href="https://www.theaterbellevue.nl/agenda/2224/Rikkert_van_Huisstede/Boys_won_039_t_be_boys/" target="_blank">Tickets</a></td>
+        <td><a href="https://www.facebook.com/events/317851759145211/" target="_blank">Facebook</a></td>
       </tr>
       <tr>
         <td>wo 16</td>
         <td>Amsterdam</td>
         <td>Theater Bellevue</td>
         <td><a href="https://www.theaterbellevue.nl/agenda/2224/Rikkert_van_Huisstede/Boys_won_039_t_be_boys/" target="_blank">Tickets</a></td>
+        <td><a href="https://www.facebook.com/events/317851759145211/" target="_blank">Facebook</a></td>
       </tr>
       <tr>
         <td>do 17</td>
         <td>Amsterdam</td>
         <td>Theater Bellevue</td>
         <td><a href="https://www.theaterbellevue.nl/agenda/2224/Rikkert_van_Huisstede/Boys_won_039_t_be_boys/" target="_blank">Tickets</a></td>
+        <td><a href="https://www.facebook.com/events/317851759145211/" target="_blank">Facebook</a></td>
       </tr>
       <tr>
         <td>vr 18</td>
         <td>Amsterdam</td>
         <td>Theater Bellevue</td>
         <td><a href="https://www.theaterbellevue.nl/agenda/2224/Rikkert_van_Huisstede/Boys_won_039_t_be_boys/" target="_blank">Tickets</a></td>
+        <td><a href="https://www.facebook.com/events/317851759145211/" target="_blank">Facebook</a></td>
       </tr>
       <tr>
         <td>za 19</td>
         <td>Amsterdam</td>
         <td>Theater Bellevue</td>
         <td><a href="https://www.theaterbellevue.nl/agenda/2224/Rikkert_van_Huisstede/Boys_won_039_t_be_boys/" target="_blank">Tickets</a></td>
+        <td><a href="https://www.facebook.com/events/317851759145211/" target="_blank">Facebook</a></td>
       </tr>
       <tr class="month-header">
         <td colspan="4">NOVEMBER 2019</td>
@@ -108,11 +123,13 @@
         <td>Arnhem</td>
         <td>Theater aan de Rijn</td>
         <td><a href="https://www.theateraanderijn.nl/programma/boys-wont-be-boys-olv-rikkert-van-huisstede" target="_blank">Tickets</a></td>
+        <td><a href="https://www.facebook.com/events/457305398406884/" target="_blank">Facebook</a></td>
       </tr>
       <tr>
         <td>n.n.b.</td>
         <td>Groningen</td>
         <td>Jonge Harten festival</td>
+        <td></td>
         <td></td>
       </tr>
       <tr class="month-header">
@@ -123,6 +140,7 @@
         <td>Den Haag</td>
         <td>Zaal 3</td>
         <td><a href="https://zaal3.nl/agenda/boys-wont-be-boys/" target="_blank">Tickets</a></td>
+        <td><a href="https://www.facebook.com/events/2335278703455597/" target="_blank">Facebook</a></td>
       </tr>
       <tr class="month-header">
         <td colspan="4">JANUARI 2020</td>
@@ -132,6 +150,7 @@
         <td>Breda</td>
         <td>Chassé theater</td>
         <td><a href="https://www.chasse.nl/theater/programma/boys-won-t-be-boys" target="_blank">Tickets</a></td>
+        <td><a href="https://www.facebook.com/events/439338940223937/" target="_blank">Facebook</a></td>
       </tr>
     </table>
   </div>
