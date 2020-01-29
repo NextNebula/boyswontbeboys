438cb4ceda158149b907bf457f4b8b2c3a0d65b1
Richard Leurs <7275740+EarthlingRich@users.noreply.github.com>
2020-01-29 15:27:38 +0100

    Add new media quote.

--- pages/index.vue
+++ pages/index.vue
@@ -1,80 +1,120 @@
 <template>
   <div>
-    <img id="top-banner" class="image-banner" src="~/assets/banner.jpg">
+    <img id="top-banner" class="image-banner" src="~/assets/banner.jpg" />
     <div id="arrow-up" class="arrow bounce"></div>
     <section id="container-agenda" class="container container-agenda">
       <div class="title">SPEELLIJST</div>
-      <agenda/>
+      <agenda />
     </section>
     <section class="container container-overview">
       <div class="title">OVER DE SHOW</div>
       <div class="text">
-        BOYS WON'T BE BOYS van Rikkert van Huisstede is een maatschappelijke beweging en theatervoorstelling die een diverse groep jongens en mannen* samenbrengt. 
-        Een collage van dansers, zangers, rappers, dichters en theatermakers laat zien dat het stoer is om je (als man) kwetsbaar op te stellen. 
-        Ze vertellen hun persoonlijke verhaal en vinden elkaar in het doorbreken van stereotypen.
-        <br>
-        <br>
-        We hebben lieve jongens nodig. Jongens die niet aan het mannelijkheidsideaal voldoen, denken vaak dat ze iets fout doen. 
-        Ze denken dat ze mannelijker zouden moeten zijn. Maar in tijden van #metoo, homofobie, seksisme, racisme en geweld zijn zij misschien juist een oplossing? Onze wereld heeft behoefte aan liefde van mannen*. 
-        Wij kunnen laten zien dat het anders kan door hokjes te doorbreken en ruimte te maken om onszelf te zijn.
-        <br>
-        <br>
-        <i>* waar wij de termen mannen/jongens gebruiken bedoelen we een breed scala aan mensen.</i>
+        BOYS WON'T BE BOYS van Rikkert van Huisstede is een maatschappelijke
+        beweging en theatervoorstelling die een diverse groep jongens en mannen*
+        samenbrengt. Een collage van dansers, zangers, rappers, dichters en
+        theatermakers laat zien dat het stoer is om je (als man) kwetsbaar op te
+        stellen. Ze vertellen hun persoonlijke verhaal en vinden elkaar in het
+        doorbreken van stereotypen.
+        <br />
+        <br />
+        We hebben lieve jongens nodig. Jongens die niet aan het
+        mannelijkheidsideaal voldoen, denken vaak dat ze iets fout doen. Ze
+        denken dat ze mannelijker zouden moeten zijn. Maar in tijden van #metoo,
+        homofobie, seksisme, racisme en geweld zijn zij misschien juist een
+        oplossing? Onze wereld heeft behoefte aan liefde van mannen*. Wij kunnen
+        laten zien dat het anders kan door hokjes te doorbreken en ruimte te
+        maken om onszelf te zijn.
+        <br />
+        <br />
+        <i
+          >* waar wij de termen mannen/jongens gebruiken bedoelen we een breed
+          scala aan mensen.</i
+        >
       </div>
     </section>
     <section class="container container-split container-first">
       <section class="container-half container-instagram">
         <div class="title" style="padding-bottom: 6px;">INSTAGRAM</div>
-        <instagram/>
+        <instagram />
       </section>
       <section class="container-half container-media">
         <div class="title" style="padding-bottom: 6px;">IN DE MEDIA</div>
         <div class="quote">
-          <i>"Herkenbaar, eigentijds, prikkelend, en eerlijk. [...] Een goede mix van sterke performers."</i>
-          <br>
+          <i
+            >"Herkenbaar, eigentijds, prikkelend, en eerlijk. [...] Een goede
+            mix van sterke performers."</i
+          >
+          <br />
           Jury Amsterdam Fringe Festival 2018
         </div>
         <div class="quote">
-          <i>"Rikkert is een innemende performer en durft zijn persoonlijke urgentie aan een breed maatschappelijk engagement te verkopen."</i>
-          <br>
-          Parool <a href="https://sanderjanssens.nl/2019/02/12/het-beste-van-dat-waar-je-niet-op-zit-te-wachten/" target="_blank">[Lees meer]</a>
+          <i
+            >"Alles bij elkaar is het een enorm project dat met veel liefde is
+            vormgegeven."</i
+          >
+          <br />
+          Theaterkrant
+          <a
+            href="https://www.theaterkrant.nl/recensie/boys-wont-be-boys/rikkert-van-huisstede/"
+            target="_blank"
+            >[Lees meer]</a
+          >
         </div>
         <div class="quote">
           Interview met regisseur Rikkert van Huisstede
-          <br>
-          <i>"De bezoeker is een beetje verliefd geworden op de jonge theatermaker. Rikkert zingt, dicht, praat en lacht op betoverende wijze over liefde, gender en seksualiteit."</i>
-          <br>
-          Gaykrant <a href="https://www.degaykrant.nl/2018/08/22/waarom-ook-mannen-jurken-mogen-dragen/" target="_blank">[Lees meer]</a>
+          <br />
+          <i
+            >"De bezoeker is een beetje verliefd geworden op de jonge
+            theatermaker. Rikkert zingt, dicht, praat en lacht op betoverende
+            wijze over liefde, gender en seksualiteit."</i
+          >
+          <br />
+          Gaykrant
+          <a
+            href="https://www.degaykrant.nl/2018/08/22/waarom-ook-mannen-jurken-mogen-dragen/"
+            target="_blank"
+            >[Lees meer]</a
+          >
         </div>
       </section>
     </section>
     <div class="container container-split container-second">
       <section class="container-half container-bel">
-      <div class="title">BEL ONS</div>
-      <div class="text">
-        We hebben een telefoonnummer: <a href="tel:0642280481">06 42 28 04 81</a>.
-        <br>Je kunt het 24/7 bellen voor inspiratie, informatie en voorproefjes van de maatschappelijke beweging en theatervoorstelling.
-      </div>
-      <div class="lijst">
-        <ul>
-          <li>doe een telefonische workshop zelfliefde (2 minuten)</li>
-          <li>beluister een opwindend gedicht van Simone van Saarloos</li>
-          <li>hoor een lied van Rikkert van Huisstede</li>
-          <li>laat Barry de Bruin je voorlezen</li>
-          <li>en heel veel meer!</li>
-        </ul>
-      </div>
-      <div class="text">
-        Eigenlijk is het telefoonnummer de voorstelling, maar dan in het klein! Probeer het zelf maar eens uit. Deze audiobeleving is gemaakt door Allard Medema.
-      </div>
+        <div class="title">BEL ONS</div>
+        <div class="text">
+          We hebben een telefoonnummer:
+          <a href="tel:0642280481">06 42 28 04 81</a>. <br />Je kunt het 24/7
+          bellen voor inspiratie, informatie en voorproefjes van de
+          maatschappelijke beweging en theatervoorstelling.
+        </div>
+        <div class="lijst">
+          <ul>
+            <li>doe een telefonische workshop zelfliefde (2 minuten)</li>
+            <li>beluister een opwindend gedicht van Simone van Saarloos</li>
+            <li>hoor een lied van Rikkert van Huisstede</li>
+            <li>laat Barry de Bruin je voorlezen</li>
+            <li>en heel veel meer!</li>
+          </ul>
+        </div>
+        <div class="text">
+          Eigenlijk is het telefoonnummer de voorstelling, maar dan in het
+          klein! Probeer het zelf maar eens uit. Deze audiobeleving is gemaakt
+          door Allard Medema.
+        </div>
       </section>
       <section class="container-half container-contact">
         <div class="title">CONTACT</div>
-        Wil je meer weten over BOYS WON'T BE BOYS? Of een interview inplannen? Dat kan allemaal. Het beeldmerk, portretten van (een aantal) spelers en de promotionele teksten zijn hieronder te vinden.
-        <br>
+        Wil je meer weten over BOYS WON'T BE BOYS? Of een interview inplannen?
+        Dat kan allemaal. Het beeldmerk, portretten van (een aantal) spelers en
+        de promotionele teksten zijn hieronder te vinden.
+        <br />
         <div class="lijst">
           <ul>
-            <li><a href="http://bit.ly/perskitBWBB" target="_blank">Download onze perskit</a></li>
+            <li>
+              <a href="http://bit.ly/perskitBWBB" target="_blank"
+                >Download onze perskit</a
+              >
+            </li>
             <li>info@wildchildagency.nl</li>
             <li>productie@zoujijhetzijn.nl</li>
             <li>rikkert@zoujijhetzijn.nl</li>
@@ -82,22 +122,39 @@
         </div>
         <div class="social">
           <a href="https://www.facebook.com/boyswontbeboys/" target="_blank">
-            <font-awesome-icon :icon="['fab', 'facebook-square']" style="font-size: 35px" />
+            <font-awesome-icon
+              :icon="['fab', 'facebook-square']"
+              style="font-size: 35px"
+            />
           </a>
-          <a href="https://www.instagram.com/boyswntbeboys/" target="_blank" style="margin-left: 0.25rem;">
-            <font-awesome-icon :icon="['fab', 'instagram']"  style="font-size: 35px" />
+          <a
+            href="https://www.instagram.com/boyswntbeboys/"
+            target="_blank"
+            style="margin-left: 0.25rem;"
+          >
+            <font-awesome-icon
+              :icon="['fab', 'instagram']"
+              style="font-size: 35px"
+            />
           </a>
         </div>
-        <div class="credits">Vormgever <a href="https://www.burobraak.nl" target="_blank">BUROBRAAK</a>. Fotograaf <a href="https://www.ernstcoppejans.com" target="_blank">Ernst Coppejans</a>.</div>
+        <div class="credits">
+          Vormgever
+          <a href="https://www.burobraak.nl" target="_blank">BUROBRAAK</a>.
+          Fotograaf
+          <a href="https://www.ernstcoppejans.com" target="_blank"
+            >Ernst Coppejans</a
+          >.
+        </div>
         <div class="foundations">BOYS WON'T BE BOYS BEDANKT</div>
-        <img class="creditLogo vsbfonds" src="~/assets/vsbfonds.png">
-        <img class="creditLogo vdef" src="~/assets/vdef.svg">
-        <img class="creditLogo afk" src="~/assets/afk.png">
-        <img class="creditLogo amerborgh" src="~/assets/amerborgh.svg">
-        <img class="creditLogo burobraak" src="~/assets/burobraak.png">
+        <img class="creditLogo vsbfonds" src="~/assets/vsbfonds.png" />
+        <img class="creditLogo vdef" src="~/assets/vdef.svg" />
+        <img class="creditLogo afk" src="~/assets/afk.png" />
+        <img class="creditLogo amerborgh" src="~/assets/amerborgh.svg" />
+        <img class="creditLogo burobraak" src="~/assets/burobraak.png" />
       </section>
     </div>
-    <img class="image-banner image-banner-bottom" src="~/assets/banner.jpg">
+    <img class="image-banner image-banner-bottom" src="~/assets/banner.jpg" />
   </div>
 </template>
 
@@ -165,25 +222,25 @@ export default {
 }
 
 .container-first {
-  background-color: #7AD4D4;
+  background-color: #7ad4d4;
 }
 
 .container-second {
-  background-color: #CAEEEE;
+  background-color: #caeeee;
 }
 
 .container-agenda {
-  background-color: #7AD4D4;
+  background-color: #7ad4d4;
   border-top: 3px solid white;
 }
 
 .container-overview {
-  background-color: #CAEEEE;
+  background-color: #caeeee;
   border-top: 3px solid white;
 }
 
 .container-instagram {
-  background-color: #7AD4D4;
+  background-color: #7ad4d4;
   border-top: 3px solid white;
   border-right: 3px solid white;
   margin-right: -3px;
@@ -192,19 +249,19 @@ export default {
 }
 
 .container-media {
-  background-color: #7AD4D4;
+  background-color: #7ad4d4;
   border-top: 3px solid white;
   border-left: 3px solid white;
 }
 
 .container-bel {
-  background-color: #CAEEEE;
+  background-color: #caeeee;
   border-right: 3px solid white;
   margin-right: -3px;
 }
 
 .container-contact {
-  background-color: #CAEEEE;
+  background-color: #caeeee;
   border-left: 3px solid white;
 }
 
@@ -233,8 +290,7 @@ export default {
   margin: 1rem auto 1rem auto;
 }
 
-.arrow
-{
+.arrow {
   z-index: 1;
   position: fixed;
   bottom: 1rem;
@@ -246,7 +302,7 @@ export default {
   animation: bounce 2s infinite;
   margin-left: auto;
   margin-right: auto;
-  filter: drop-shadow( 1px 1px 1px rgba(0, 0, 0, .5));
+  filter: drop-shadow(1px 1px 1px rgba(0, 0, 0, 0.5));
 }
 
 .foundations {
@@ -279,19 +335,17 @@ export default {
 
   .container-media {
     border-left: none;
-    background-color: #CAEEEE;
+    background-color: #caeeee;
   }
-  
 
   .container-bel {
     border-right: none;
     border-bottom: 3px solid white;
-    background-color: #7AD4D4;
+    background-color: #7ad4d4;
   }
 }
 
-@media only screen and (max-width: 540px)
-{
+@media only screen and (max-width: 540px) {
   .creditLogo {
     display: block;
     margin-left: auto;
@@ -304,7 +358,11 @@ export default {
 }
 
 @keyframes bounce {
-  0%, 20%, 50%, 80%, 100% {
+  0%,
+  20%,
+  50%,
+  80%,
+  100% {
     transform: translateY(0);
   }
   40% {
