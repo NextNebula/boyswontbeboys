e5a7fa254529f680627d17cbb4731892673e5ded
Richard Leurs <leursrichard@gmail.com>
2021-06-22 22:39:55 -0400

    Add vacature page.

---
+++ pages/vacatures.vue
@@ -0,0 +1,128 @@
+<template>
+    <div>
+        <div class="container container-title">
+            <section class="section-even">
+                <h1 class="page-title">Vacatures</h1>
+                BOYS WON’T BE BOYS heeft dit moment 2 vacatures. Een vacature voor <a href="#productieleider">productieleider</a> en een vacture voor <a href="#lichttechnicus">lichttechnicus</a>.
+            </section>
+        </div>
+        <div class="container container-vacature-1">
+            <section class="section-even">
+                <h2 id="productieleider" class="vacature-title">Productieleider (m/v/x)</h2>
+                BOYS WON’T BE BOYS zoekt een nieuwe Productieleider voor de theatertournee! De voorstellingen zijn van september-december 2021, met eventueel extra data in 2022. Voorbereidingen beginnen het liefst zo snel mogelijk. Word jij de nieuwe, lieve spin in ons web?
+                <br><br>
+                <div class="list-text">
+                    Werkzaamheden zijn o.a.:
+                    <ul>
+                        <li>Het plannen, roosteren en organiseren van de theatertour</li>
+                        <li>Contact met theaters over techniek en zaalopstelling</li>
+                        <li>Contact met de spelers over planning en organisatie</li>
+                        <li>Op speeldagen aanwezig en verantwoordelijk voor: vervoer, zaalopstelling, kleedkamers, etc.</li>
+                        <li>Praktische ondersteuning van de gehele organisatie</li>
+                    </ul>
+                </div>
+                <br>
+                <div class="list-text">
+                    We zoeken een productieleider die:
+                    <ul>
+                        <li>Een talent heeft voor roosteren, plannen en organiseren</li>
+                        <li>Optimistisch is, met humor en relativeringsvermogen</li>
+                        <li>Stressbestendig en flexibel is en zelfstandig werkt</li>
+                        <li>Bij voorkeur ervaring heeft met theatertournees</li>
+                        <li>Binding heeft met de thematiek mannelijkheid en lhbti+</li>
+                        <li>Woonachtig is in Amsterdam</li>
+                        <li>Bij voorkeur een rijbewijs/eigen auto tot de beschikking heeft</li>
+                    </ul>
+                </div>
+                <br>
+                Geïnteresseerd?<br>
+                Mocht je geïnteresseerd zijn, stuur dan je motivatie met CV zo snel mogelijk (uiterlijk  30 juni) naar Rikkert@zoujijhetzijn.nl o.v.v. Vacature Productieleider.
+                De gesprekken vinden plaats in de week van 28 juni. De functie is op ZZP basis. N.B. We streven naar een cultureel divers team. Als je nog twijfelt of vragen hebt mag je altijd contact opnemen met Rikkert.
+            </section>
+        </div>
+        <div class="container container-vacature-2">
+            <section class="section-even">
+            <h2 id="lichttechnicus" class="vacature-title">Lichttechnicus (m/v/x)</h2>
+            BOYS WON’T BE BOYS zoekt een nieuwe lichttechnicus voor de theatertournee! De voorstellingen zijn van september-december 2021, met eventueel extra data in 2022. Zet jij al onze spelers mooi in het licht?
+            <br><br>
+            <div class="list-text">
+                Werkzaamheden zijn:
+                <ul>
+                    <li>Het voorbereiden, inhangen en draaien van het lichtplan</li>
+                    <li>Per voorstelling creatief omgaan met locatie en zaalopstelling</li>
+                    <li>N.B.wij spelen een voorstelling die elke keer anders is met wisselende acts</li>
+                </ul>
+            </div>
+            <br>
+            <div class="list-text">
+                We zoeken een technicus die:
+                <ul>
+                    <li>Stressbestendig en flexibel is en zelfstandig werkt</li>
+                    <li>het leuk vindt om creatief in te spelen op veranderingen</li>
+                    <li>Binding heeft met de thematiek mannelijkheid en lhbti+</li>
+                    <li>Ervaring heeft met theatertournees</li>
+                    <li>In het bezit van een rijbewijs/eigen auto tot de beschikking heeft</li>
+                </ul>
+            </div>
+            <br>
+            Geïnteresseerd?<br>
+            Mocht je geïnteresseerd zijn, stuur dan je motivatie met CV zo snel mogelijk (uiterlijk  30 juni) naar Rikkert@zoujijhetzijn.nl o.v.v. Vacature Lichttechnicus. De gesprekken vinden plaats in de week van 28 juni. De functie is op ZZP basis. N.B. We streven naar een cultureel divers team. Als je nog twijfelt of vragen hebt mag je altijd contact opnemen met Rikkert.
+            </section>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style>
+    .container {
+        padding-top: 20px;
+        padding-bottom: 20px;
+        padding-left: 10px;
+        padding-right: 10px;
+    }
+
+    .container-title {
+        background-color: #7ad4d4;
+    }
+
+    .container-vacature-1 {
+        background-color: #caeeee;
+        border-top: 3px solid white;
+        border-bottom: 3px solid white;
+    }
+
+    .container-vacature-2 {
+        background-color: #7ad4d4;
+    }
+
+    section {
+        max-width: 800px;
+        margin: auto;
+        text-align: center;
+    }
+
+    .list-text {
+        text-align: left;
+        margin-left: 10%;
+    }
+
+    @media only screen and (max-width: 600px) {
+        .list-text {
+            margin-left: 5%;
+        }
+    }
+
+    .page-title {
+        margin-bottom: 5px;
+        font-size: 40px;
+    }
+
+    .vacature-title {
+        margin-bottom: 5px;
+        font-size: 30px;
+    }
+</style>
